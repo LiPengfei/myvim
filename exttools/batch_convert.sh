ad59217784e1beacec413deaf68f950b8337ad7d
lipengfei <503112347@qq.com>
2015-05-16 21:42:08 +0800

    add dos2unix tool

---
+++ exttools/batch_convert.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function batch_convert(){
+	for file in `ls $1`
+	do
+		if [ -d $1"/"$file ]
+		then
+			batch_convert $1"/"$file
+		else
+			dos2unix $1"/"$file
+		fi
+	done
+}
+
+batch_convert ~/.vim
+batch_convert ~/vundle
